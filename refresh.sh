a4534d69e71665b987d64d8d0758298dc8a2a186
makansij <jordan.makansi@bucknell.edu>
2025-09-30 21:36:05 -0400

    adding bash scripts refresh.sh and remove_old.sh.

---
+++ refresh.sh
@@ -0,0 +1,4 @@
+cp Module_2.3_Visualize_Quantum_States.ipynb Module_2.3_Visualize_Quantum_States_backup.ipynb 
+echo "old file moved to backup"
+cp ~/Downloads/Module_2.3_Visualize_Quantum_States.ipynb .
+echo "new file copied to current directory"
---
+++ remove_old.sh
@@ -0,0 +1,6 @@
+file="/Users/jordanmakansi/Downloads/Module_2.3_Visualize_Quantum_States.ipynb"
+if [ -f "$file" ] ; then
+    rm "$file"
+    echo "old file removed from downloads"
+fi
+
